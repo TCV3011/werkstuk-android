b51aeab3c5e54a07cea746ca2ca40d73afdd9ce2
tychoverstraete <tycho.verstraete@student.ehb.be>
2021-08-21 02:42:51 +0200

    Add a note to Database

--- WerkstukAndroid/app/src/main/java/be/ehb/werkstukandroid/AddNote.java
+++ WerkstukAndroid/app/src/main/java/be/ehb/werkstukandroid/AddNote.java
@@ -3,12 +3,30 @@ package be.ehb.werkstukandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import be.ehb.werkstukandroid.database.Database;
+import be.ehb.werkstukandroid.database.DatabaseRepository;
+import be.ehb.werkstukandroid.entity.Note;
 
 public class AddNote extends AppCompatActivity {
+    EditText noteTitle, noteDetails;
+    DatabaseRepository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("New note");
         setContentView(R.layout.activity_add_note);
+    }
+
+    public void saveNote(View view) {
+        repo = new DatabaseRepository(this.getApplication());
+        noteTitle = findViewById(R.id.noteTitle);
+        noteDetails = findViewById(R.id.noteDetails);
+
+        repo.insertNote(new Note(noteTitle.getText().toString(), noteDetails.getText().toString()));
     }
 }
--- WerkstukAndroid/app/src/main/java/be/ehb/werkstukandroid/MainActivity.java
+++ WerkstukAndroid/app/src/main/java/be/ehb/werkstukandroid/MainActivity.java
@@ -7,15 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().setTitle("My notes");
         recyclerView = findViewById(R.id.ListOfNotes);
     }
 
